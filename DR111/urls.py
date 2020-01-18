e3606ce1998658c337e97932375971d05e1754aa
pranavkulkarni <pranav.kulkarni54@gmail.com>
2020-01-18 20:07:05 +0530

    Searching Partially completed

--- DR111/urls.py
+++ DR111/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('alumni/',views.alumlogin, name='alumni'),
     path('clg/',views.clglogin,name='clg'),
+    path('collegeLogin/', views.postclglogin, name='postclglogin'),
     path('dte/',views.dtlogin,name='dte'),
     path('postsign/',views.postsign),
     path('logout/',views.logout,name="log"),
@@ -38,8 +39,8 @@ urlpatterns = [
     path('professional/',views.professional,name="prof"),
     path('alumniregistration/',views.register,name="alumniregistration_1"),
     path('dir/',views.dir,name="dir"),
-    path('collsearch/',views.collsearch,name="collsearch")
-
+    path('collsearch/',views.collsearch,name="collsearch"),
+    path('postcollsearch/', views.postcollsearch, name="postcollsearch"),
   #  path('password_reset/$',auth_views.password_reset,name="password_reset"),
   #  path('password_reset/done/$',auth_views.password_reset_done,name="password_reset_done"),
    # path('password_reset/confirm/(?p<uidb64>[\w-]+)/(?p<token>[\w-]+)/$',auth_views.password_reset_confirm,name="password_reset_confirm"),
--- DR111app/views.py
+++ DR111app/views.py
@@ -1,16 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import pyrebase
 from django.contrib import auth
 
-
-
-
-
-
-
-
 config = {
-    'apiKey' : "AIzaSyAFpFI-PLwWMQBQl1Sa8Xcr8lDtX8HjACw",
+    'apiKey': "AIzaSyAFpFI-PLwWMQBQl1Sa8Xcr8lDtX8HjACw",
     'authDomain': "alumni-tracking-dr111.firebaseapp.com",
     'databaseURL': "https://alumni-tracking-dr111.firebaseio.com",
     'projectId': "alumni-tracking-dr111",
@@ -20,72 +13,154 @@ config = {
     'measurementId': "G-H6GZ2F9TNS"
 
 }
-firebase=pyrebase.initialize_app(config)
-authe=firebase.auth()
-database=firebase.database()
-
+firebase = pyrebase.initialize_app(config)
+authe = firebase.auth()
+database = firebase.database()
 
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
 def alumlogin(request):
-    return render(request,'alumlogin.html')
+    return render(request, 'alumlogin.html')
+
+
 def clglogin(request):
-    return render(request,'clglogin.html')
+    return render(request, 'clglogin.html')
+
+
 def dtlogin(request):
-    return render(request,'dtlogin.html')
+    return render(request, 'dtlogin.html')
+
+
 def postsign(request):
-    email=request.POST.get('uname')
+    email = request.POST.get('uname')
     passw = request.POST.get('psw')
     try:
-     user = authe.sign_in_with_email_and_password(email,passw)
+        user = authe.sign_in_with_email_and_password(email, passw)
     except:
-        message="Invalid Credentials"
-        return render(request,"alumlogin.html",{"messg":message})
-   # print(user['idToken'])
-   # session_id=user['idToken']
-   # a=authe.get_account_info(user['idToken'])
-   # print(a)
-   # a=a['users']
-   # a=a[0]
-    #print(a['localId'])
-   # a=a['localId']
-   # print(database.child('COLLEGES').child(a).get().val())
-   # request.session['uid']=str(session_id)
-    return render(request,'welcome.html',{"e":email})
-def logout(request):
-    auth.logout(request)
-    return render(request,'alumlogin.html')
-def Cancel(request):
-    return render(request,'index.html')
-def password_reset(request):
-    return render(request,'password_reset_form.html')
-def password_reset_done(request):
-    return render(request,'password_reset_done.html')
-def password_reset_confirm(request):
-    return render(request,'password_reset_confirm.html')
-def password_reset_complete(request):
-    return render(request,'password_reset_complete.html')
-def register(request):
-    return render(request,'alumniregistration_1.html')
-def academic(request):
-    return render(request,'academic.html')
-def professional(request):
-    return render(request,'professional.html')
-def collsearch(request):
-    return render(request,'collsearch.html')
-def dir(request):
-    return render(request,'dir.html')
-def postsignup(request):
-    name=request.POST.get('name')
-    email=request.POST.get('email')
-    passw=request.POST.get('password')
-
-    user=authe.create_user_with_email_and_password(email,passw)
+        message = "Invalid Credentials"
+        return render(request, "alumlogin.html", {"messg": message})
     uid = user['localId']
-    data={"name":name,"status":"1"}
+    accountInfo = authe.get_account_info(user['idToken'])
+    accountInfo = accountInfo['users']
+    accountInfo = accountInfo[0]
+    accountInfo = accountInfo['emailVerified']
+    if accountInfo:
+        return render(request, 'welcome.html', {"e": email})
+    return render(request, "alumlogin.html", {"messg": 'Email ID is not verified '})
+
+
+# print(user['idToken'])
+# a=authe.get_account_info(user['idToken'])
+# print(a)
+# a=a['users']
+# a=a[0]
+# print(a['localId'])
+# a=a['localId']
+# print(database.child('COLLEGES').child(a).get().val())
+
+
+def logout(request):
+    try:
+        del request.session['uid']
+    except:
+        pass
+    return redirect('/')
+
+
+def Cancel(request):
+    return render(request, 'index.html')
+
+
+def password_reset(request):
+    return render(request, 'password_reset_form.html')
+
+
+def password_reset_done(request):
+    return render(request, 'password_reset_done.html')
+
+
+def password_reset_confirm(request):
+    return render(request, 'password_reset_confirm.html')
+
+
+def password_reset_complete(request):
+    return render(request, 'password_reset_complete.html')
+
+
+def register(request):
+    return render(request, 'alumniregistration_1.html')
+
+
+def academic(request):
+    return render(request, 'academic.html')
+
+
+def professional(request):
+    return render(request, 'professional.html')
+
+
+def collsearch(request):
+    return render(request, 'collsearch.html')
+
+
+def dir(request):
+    return render(request, 'dir.html')
+
+
+def postsignup(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    passw = request.POST.get('password')
+    college = request.POST.get('clgname')
+    passing_year = request.POST.get('passout')
+    branch = request.POST.get('branch')
+    print(college)
+    user = authe.create_user_with_email_and_password(email, passw)
+    uid = user['localId']
+    authe.send_email_verification(user['idToken'])
+    data = {"NAME": name, "EMAIL": email, "REGISTERED": False, "PASSOUT": passing_year, 'BRANCH': branch, }
+    database.child('COLLEGES').child(college).child('STUDENT DETAILS').child(uid).child('PERSONAL DETAILS').set(data)
     database.child("users").child(uid).child("details").set(data)
-    return render(request,"alumlogin.html")
+    database.child('COLLEGES').child('REGISTERED').child(uid).set(True)
+    return render(request, "alumlogin.html")
 
 
+def postcollsearch(request):
+    uid = request.POST.get('uid')
+    passout = request.POST.get('passout')
+    name = request.POST.get('sname')
+    branch = request.POST.get('branch')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    college = database.child('uid').child(uid).get().val()
+    keys = database.child('COLLEGES').child(college).child('STUDENT DETAILS').shallow().get().val()
+    flag = 0
+    for i in keys:
+        if database.child('COLLEGES').child(college).child('STUDENT DETAILS').child(i).child('PERSONAL DETAILS').child(
+                'email').get().val() == email and database.child('COLLEGES').child(college).child(
+                'STUDENT DETAILS').child(i).child('PERSONAL DETAILS').child('name').get().val().lower() == name.lower():
+            print("got you mmffffff")
+            flag = 1
+            break
+    print("flag", flag)
+    print("i am here", uid, email)
+
+    return redirect('/')
+
+
+def postclglogin(request):
+    email = request.POST.get('uname')
+    password = request.POST.get('psw')
+    try:
+        user = authe.sign_in_with_email_and_password(email, password)
+    except:
+        message = "Invalid Credentials"
+        print("I am in here")
+        return render(request, "clglogin.html", {"messg": message})
+    clguid = user['localId']
+    print(clguid)
+    return render(request, 'collsearch.html', {'uid': clguid})
